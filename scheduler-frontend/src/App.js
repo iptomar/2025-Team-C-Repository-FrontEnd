8dbe9da01d80f721a39bc3376fe3464128719cf3
Daniel Marques <daniel23marq@gmail.com>
2025-04-04 18:30:16 +0100

    Criação de uma homepage e estilo para a mesma.

--- scheduler-frontend/src/App.js
+++ scheduler-frontend/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+//import Login from './Pages/Login'; // Página de Login
+//import CRUDInt from './Pages/CRUDInt'; // Página de CRUD
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Gestão de Horários</h1>
+          <p>Bem-vindo à aplicação de gestão de horários.</p>
+          <div className="button-container">
+            {/* Botão para a página de Login */}
+            <Link to="/login">
+              <button className="main-button">Login</button>
+            </Link>
+            {/* Botão para a página de CRUD */}
+            <Link to="/crud">
+              <button className="main-button">CRUD</button>
+            </Link>
+          </div>
+        </header>
+        {/* <Switch> 
+           //Rota para a página de Login 
+          <Route path="/login" component={Login} />
+           //Rota para a página de CRUD 
+          <Route path="/crud" component={CRUDInt} />
+        </Switch> */}
+      </div>
+    </Router>
   );
 }
 
