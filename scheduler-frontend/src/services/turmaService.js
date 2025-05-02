f42b2c00d4ace2b8bbfd1281bac087003e2e4f7c
Francisco Pires <franciscop11@outlook.pt>
2025-05-02 14:11:38 +0100

    Consumo dos endpoints referentes às turmas e UCs.

---
+++ scheduler-frontend/src/services/turmaService.js
@@ -0,0 +1,14 @@
+import api from './api';
+
+// Serviço para interagir com a API de turmas.
+// Este serviço contém funções para obter, criar, atualizar e excluir turmas.
+const turmaService = {
+  getAll: () => api.get('/ApiTurmas/GetAll'),
+  GetByDisciplina: (id) => api.get(`/ApiTurmas/GetByDisciplina/${id}`), // Aqui busca por ID da disciplina
+  GetById: (id) => api.get(`/ApiTurmas/GetById/${id}`), // Aqui busca por ID da turma
+  create: (turma) => api.post('/ApiTurmas/Create', turma),
+  update: (id, turma) => api.put(`/ApiTurmas/Update/${id}`, turma), // Edita a sala pelo o ID da turma
+  delete: (id) => api.delete(`/ApiTurmas/Delete/${id}`) // Apaga a sala pelo o ID da turma
+};
+
+export default turmaService;
